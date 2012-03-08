258fca75ba36891b5bcfe6225146a1d05b3d8ede
Timothy Baldridge <tbaldridge@gmail.com>
2012-03-08 13:07:53 -0600

    work on MapEntry

---
+++ clojure/lang/amapentry.py
@@ -0,0 +1,45 @@
+from clojure.lang.apersistentvector import APersistentVector
+from clojure.lang.persistentvector import create as createVector
+from clojure.lang.cljexceptions import IndexOutOfBoundsException
+
+
+class AMapEntry(APersistentVector):
+    def nth(self, i):
+        if i == 0:
+            return self.getKey()
+        elif i == 1:
+            return self.getValue()
+        else:
+            raise IndexOutOfRangeException()
+    
+    def asVector(self):
+        return createVector(self.getKey(), self.getValue())
+        
+    def assocN(self, i, val):
+        return self.asVector().assocN(i val)
+        
+    def __len__(self):
+        return 2
+        
+        
+    def seq(self):
+        return self.asVector().seq()
+        
+        
+    def cons(self, o):
+        return self.asVector().cons(o)
+        
+    def empty(self):
+        return None
+        
+    def pop():
+        return createVector(self.getKey())
+        
+        
+        
+        
+
+
+
+
+
