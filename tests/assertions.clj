8972908330d38f30f80c607c0586c09083c0e6d5
Eric Shull <eric.shull@gmail.com>
2012-03-13 14:09:07 -0400

    Refactoring in assertions utils.

--- tests/assertions.clj
+++ tests/assertions.clj
@@ -1,24 +1,29 @@
 (ns tests.assertions)
 
+
+(defn fail [& msgs]
+  (throw (py/AssertionError (apply str msgs))))
+
+
 (defn assert-true [val]
     (if val true
-            (throw (py/AssertionError (str val " is not true")))))
+            (fail val " is not true")))
 
 (defn assert-false [val]
-    (if val (throw (py/AssertionError (str val " is not false")))
+    (if val (fail val " is not false")
             false))
 
 (defn assert-greater [x y]
     (if (py.bytecode/COMPARE_OP ">" x y)
         true
-        (throw (py/AssertionError (str x " is not greater than " y)))))
+        (fail x " is not greater than " y)))
 
 (defn assert-lesser [x y]
     (if (py.bytecode/COMPARE_OP "<" x y)
         true
-        (throw (py/AssertionError (str x " is not greater than " y)))))
+        (fail x " is not less than " y)))
 
 (defn assert-equal [x y]
     (if (py.bytecode/COMPARE_OP "==" x y)
         true
-        (throw (py/AssertionError (str x " does not equal " y)))))
+        (fail x " does not equal " y)))
