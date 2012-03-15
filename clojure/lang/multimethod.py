522cc3c04095f1857a00f56b013208a7dc771e7a
halgari <tbaldridge@gmail.com>
2012-03-15 11:09:57 +0800

    start of multimethods

---
+++ clojure/lang/multimethod.py
@@ -0,0 +1,36 @@
+from clojure.lang.cljkeyword import keyword
+
+default = keyword("default")
+
+class MultiMethodException(Exception):
+    def __init__(self, reason):
+        Exception.__init__(self, reason)
+
+class MultiMethod(object):
+    def __init__(self, selector):
+        self.selector = selector
+        self.fns = {}
+        self.default = None
+        
+    def addMethod(self, value, fn):
+        if value in self.fns:
+            raise MultiMethodException("Method already exists for value " + str(value))
+        if value == default and self.default is not none:
+            raise MultiMethodException("Method already exists for value " + str(value))
+
+        if value == default:
+            self.default = fn
+        else:
+            self.fns[value]
+            
+    def __call__(self, *args):
+        dval = self.selector(*args)
+        type:
+            fn = self.fns[dval]
+        except KeyError:
+            if not self.default:
+                return self.default(*args)
+            raise MultiMethodException("No match for dispatch value " + str(value))
+        
+        return fn(*args)
+
