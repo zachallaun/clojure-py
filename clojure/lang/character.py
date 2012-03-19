0609b4cd63bf31339796176b52f39de1a77b34d0
stirfoo <stirfoo@gmail.com>
2012-03-18 23:02:26 -0700

    mucking about with Character

--- clojure/lang/character.py
+++ clojure/lang/character.py
@@ -18,6 +18,17 @@ charToSymbol = {
     "\r" : "return",
     }
 
+# associated core functions:
+# * char?
+# * char
+# * char-array
+# * chars
+
+# associated core-print functions
+# * char-escape-string
+
+# TODO: maybe store the charcter internally as an integer codepoint
+
 class Character(IPrintable):
     """A single character.
 
@@ -31,14 +42,17 @@ class Character(IPrintable):
     def __init__(self, c):
         self._c = c.encode("utf-8")
     def __hash__(self):
-        return hash(self.c)
+        return hash(self._c)
+    # quack quack
+    def __int__(self):
+        return ord(self._c.decode("utf-8"))
     # XXX: temporary until writeAsString and writeAsReplString come on line
     def __repr__(self):
-        return "\\" + charToSymbol.get(self.c, self.c)
+        return "\\" + charToSymbol.get(self._c, self._c)
     def writeAsString(self, writer):
-        writer.write(self.c)
+        writer.write(self._c)
     def writeAsReplString(self, writer):
-        writer.write("\\" + charToSymbol.get(self.c, self.c))
+        writer.write("\\" + charToSymbol.get(self._c, self._c))
     @property
     def c(self):
         """Return the internal one string character."""
@@ -47,18 +61,21 @@ class Character(IPrintable):
 # This is but a start to a Character implementation.
 # I think this might be the overhead halgari was alluding to.
 # It's not thread safe either.
-        
+
+# "x" => Character
 _charCache = {}
 
 def character(c):
     """Return a Character instance set to the one character Python string c.
 
-    Character instance are cached so:
+    Character instances are cached so:
     character("x") is character("x") => True
     """
     wref = _charCache.get(c)
-    if wref and wref() is not None:
-        return wref()
+    if wref:
+        o = wref()
+        if o is not None:
+            return o
     ch = Character(c)
     _charCache[c] = weakref.ref(ch)
     for k in list(_charCache):
