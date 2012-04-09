c1aeb2f43f70c06281aaea731b81f4807fcc0ce9
Adam Feuer <adamf@pobox.com>
2012-04-08 20:06:08 -0700

    removed re-seq from untranslated.clj

--- clojure/untranslated.clj
+++ clojure/untranslated.clj
@@ -160,18 +160,6 @@
         (do (f) :ok)
         :no-test)))
 
-(defn re-seq
-  "Returns a lazy sequence of successive matches of pattern in string,
-  using java.util.regex.Matcher.find(), each such match processed with
-  re-groups."
-  {:added "1.0"
-   :static true}
-  [^java.util.regex.Pattern re s]
-  (let [m (re-matcher re s)]
-    ((fn step []
-       (when (. m (find))
-         (cons (re-groups m) (lazy-seq (step))))))))
-
 (defn re-matches
   "Returns the match, if any, of string to pattern, using
   java.util.regex.Matcher.matches().  Uses re-groups to return the
