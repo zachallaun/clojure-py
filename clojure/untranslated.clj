da65d5f246c5719780351e4eb9d7b6cd44527449
Adam Feuer <adamf@pobox.com>
2012-04-12 20:38:06 -0700

    removed re-find from untranslated.clj

--- clojure/untranslated.clj
+++ clojure/untranslated.clj
@@ -160,19 +160,6 @@
         (do (f) :ok)
         :no-test)))
 
-(defn re-find
-  "Returns the next regex match, if any, of string to pattern, using
-  java.util.regex.Matcher.find().  Uses re-groups to return the
-  groups."
-  {:added "1.0"
-   :static true}
-  ([^java.util.regex.Matcher m]
-   (when (. m (find))
-     (re-groups m)))
-  ([^java.util.regex.Pattern re s]
-   (let [m (re-matcher re s)]
-     (re-find m))))
-
 (defn rand
   "Returns a random floating point number between 0 (inclusive) and
   n (default 1) (exclusive)."
