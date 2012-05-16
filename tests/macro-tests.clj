d6ad82f8726196609bd9d2c5154c110ed792ed39
Konrad Hinsen <konrad.hinsen@fastmail.net>
2012-05-16 09:47:52 +0200

    Updated macroexpand tests

--- tests/macro-tests.clj
+++ tests/macro-tests.clj
@@ -5,14 +5,24 @@
 ; This test depends on macro definitions in clojure.core,
 ; which might change in the future.
 (deftest core-expansion-tests
-   (assertions/assert-equal (macroexpand-1 '(when a b)) '(if a (do b)))
-   (assertions/assert-equal (macroexpand '(when a b)) '(if* a (do b))))
+  (assertions/assert-equal (macroexpand-1 '(when a b))
+                           '(clojure.core/if a (do b)))
+  (assertions/assert-equal (macroexpand '(when a b))
+                           '(if* a (do b))))
+
+(defmacro foo1 [& body] (cons 'do body))
+(defmacro foo2 [& body] (list 'foo1 (cons 'foo1 body)))
+
+; A brute-force test in order to create some failure
+; if there's a bug in macroexpand-1. For now, macroexpand
+; doesn't work correctly inside tests because *ns* is not
+; set correctly.
+(assert (= (macroexpand-1 '(foo1 a b c)) '(do a b c)))
+(assert (= (macroexpand-1 '(foo2 a b c)) '(foo1 (foo1 a b c))))
+(assert (= (macroexpand '(foo2 a b c)) '(do (foo1 a b c))))
 
 (comment
-  ; This test doesn't work yet because macroexpand doesn't have
-  ; access to the namespace in which it was called at compilation time.
-  (defmacro foo1 [& body] `(do ~@body))
-  (defmacro foo2 [& body] `(foo1 (foo1 ~@body)))
+  ; This is how it should be done.
   (deftest expansion-tests
     (assertions/assert-equal (macroexpand-1 '(foo1 a b c)) '(do a b c))
     (assertions/assert-equal (macroexpand-1 '(foo2 a b c)) '(foo1 (foo1 a b c)))
