d16fccc835a63c95c0422dfafcbf5bb73f730c25
halgari <tbaldridge@gmail.com>
2012-05-21 22:29:32 -0500

    ifn is now built via treadle

--- clojure/lang/ifn.py
+++ clojure/lang/ifn.py
@@ -1,5 +1,21 @@
 from clojure.lang.cljexceptions import AbstractMethodCall
+from clojure.util.treadle import treadle as tr
 
-class IFn(object):
-    def __call__(self, *args):
-        raise AbstractMethodCall(self)
+MAX_IFN_ARITY = 20
+
+def _init():
+    invokes = {}
+    
+    def make_arg(x):
+        return tr.Argument("arg"+str(x))
+    
+    for x in range(MAX_IFN_ARITY):
+        slf = tr.Argument("self")
+        args = [slf] + map(make_arg, range(x))
+        fn = tr.Func(args, tr.Raise(tr.Call(tr.Const(AbstractMethodCall), slf)))
+        invokes["invoke"+str(x)] = fn.toFunc()
+        
+    globals()["IFn"] = type("IFn", (object,), invokes)
+                    
+_init()
+        
