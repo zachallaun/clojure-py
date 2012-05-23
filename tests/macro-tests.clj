525b4428ba8b2f1e334d3a6c11d6bf2beaa98e89
Konrad Hinsen <konrad.hinsen@fastmail.net>
2012-05-23 18:49:54 +0200

    Updated macro-tests

--- tests/macro-tests.clj
+++ tests/macro-tests.clj
@@ -1,6 +1,6 @@
 (ns tests.macro-tests
     (:require [tests.assertions :as assertions])
-    (:require [tests.utils :only [deftest]]))
+    (:use [tests.utils :only [deftest]]))
 
 ; This test depends on macro definitions in clojure.core,
 ; which might change in the future.
@@ -10,21 +10,15 @@
   (assertions/assert-equal (macroexpand '(when a b))
                            '(if* a (do b))))
 
-(defmacro foo1 [& body] (cons 'do body))
-(defmacro foo2 [& body] (list 'foo1 (cons 'foo1 body)))
+(defmacro foo1 [& body] `(do ~@body))
+(defmacro foo2 [& body] `(foo1 (foo1 ~@body)))
 
-; A brute-force test in order to create some failure
-; if there's a bug in macroexpand-1. For now, macroexpand
-; doesn't work correctly inside tests because *ns* is not
-; set correctly.
-(assert (= (macroexpand-1 '(foo1 a b c)) '(do a b c)))
-(assert (= (macroexpand-1 '(foo2 a b c)) '(foo1 (foo1 a b c))))
-(assert (= (macroexpand '(foo2 a b c)) '(do (foo1 a b c))))
-
-(comment
-  ; This is how it should be done.
-  (deftest expansion-tests
-    (assertions/assert-equal (macroexpand-1 '(foo1 a b c)) '(do a b c))
-    (assertions/assert-equal (macroexpand-1 '(foo2 a b c)) '(foo1 (foo1 a b c)))
-    (assertions/assert-equal (macroexpand '(foo2 a b c)) '(do (foo1 a b c)))))
+(deftest expansion-tests
+  (assertions/assert-equal (macroexpand-1 '(foo1 a b c))
+                           '(do a b c))
+  (assertions/assert-equal (macroexpand-1 '(foo2 a b c))
+                           '(tests.macro-tests/foo1
+                             (tests.macro-tests/foo1 a b c)))
+  (assertions/assert-equal (macroexpand '(foo2 a b c))
+                           '(do (tests.macro-tests/foo1 a b c))))
 
