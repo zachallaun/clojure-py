80ba5e525cdbaf42fccdb6e9de28181fdb8e80ae
Zachary Allaun <zach.allaun@gmail.com>
2012-07-23 17:24:10 -0400

    Translate assoc-in

--- clojure/untranslated.clj
+++ clojure/untranslated.clj
@@ -689,19 +689,6 @@
     (load-one lib true true))
   lib)
 
-;;;;;;;;;;;;; nested associative ops ;;;;;;;;;;;
-
-(defn assoc-in
-  "Associates a value in a nested associative structure, where ks is a
-  sequence of keys and v is the new value and returns a new nested structure.
-  If any levels do not exist, hash-maps will be created."
-  {:added "1.0"
-   :static true}
-  [m [k & ks] v]
-  (if ks
-    (assoc m k (assoc-in (get m k) ks v))
-    (assoc m k v)))
-
 (defn empty?
   "Returns true if coll has no items - same as (not (seq coll)).
   Please use the idiom (seq x) rather than (not (empty? x))"
